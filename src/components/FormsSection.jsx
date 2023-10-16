1f70ebdd750887b488c5b6aca78e3b8fddd2348c
Rios-101 <osuyarios@gmail.com>
2023-10-17 00:50:03 +0100

    first commit

--- src/components/FormsSection.jsx
+++ src/components/FormsSection.jsx
@@ -1,44 +1,80 @@
-import React, { useState } from "react";
-import { navTab } from "../data";
+import React from "react";
 
 const FormsSection = () => {
   const style = {
     fontFamily: "'Pacifico', cursive",
-    color: "#5060A9",
-  };
-
-  const [activeTab, setActiveTab] = useState(navTab[0]); // Set the initial active tab
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+    // color: "#303555",
   };
 
   return (
-    <div className="w-[100%] lg:w-[35%]  bg-[#F3F6FD]">
-      <h2 style={style} className=" p-8 text-[30px]">
-        Thrillers Travels
-      </h2>
-      <div className="grid grid-cols-4 gap-1">
-        {navTab.map((ele) => {
-          const isActive = ele.id === activeTab.id;
-          return (
-            <div
-              key={ele.id}
-              className={`text-center  p-4 cursor-pointer ${
-                isActive
-                  ? "bg-[#73C684] text-white shadow-[rgba(0,_0,_0,_0.2)_0px_10px_40px_-7px]"
-                  : "text-[#C1C4DA] bg-white hover:bg-[#73C684] hover:text-white"
-              }`}
-              onClick={() => handleTabClick(ele)}
-            >
-              <i className={ele.font}></i>
-              <p>{ele.name}</p>
+    <div className="w-[100%] flex flex-col justify-between lg:w-[20%]  bg-[#F3F6FD]">
+      <div>
+        <h2
+          style={style}
+          className=" pt-8 pb-2 px-14 text-center lg:text-start text-[#303555] text-[30px] "
+        >
+          Mima<span className="text-[#696969]">Booking</span>
+        </h2>
+        <div className="flex justify-center">
+          <div class=" mr-2 w-20 h-20 relative flex justify-center items-center rounded-full text-xl text-white border-2 p-1 border-[#2d3253]">
+            <img
+              src="http://source.unsplash.com/100x100/?woman"
+              className="rounded-full"
+              alt="prfile"
+            />
+            <div className="absolute flex justify-center items-center -bottom-3 w-6 h-6 rounded-full bg-[#2d3253]">
+              <i className="pl-1 text-[10px] far fa-edit "></i>
             </div>
-          );
-        })}
+          </div>
+        </div>
+        <h2
+          style={style}
+          className="text-center pt-6 text-[#303555] text-[20px]"
+        >
+          Ben Grim
+        </h2>
       </div>
-      {/* Render the active tab's content */}
-      {activeTab.component}
+      <div className="pl-10">
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-home text-[#2d3253]"></i>
+          <h3 style={style} className="text-[#2d3253]">
+            Dashboard
+          </h3>
+        </div>
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-dot-circle text-[#696969]"></i>
+          <h3 style={style} className="text-[#696969]">
+            Explore City
+          </h3>
+        </div>
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-inbox text-[#696969]"></i>
+          <h3 style={style} className="text-[#696969]">
+            Ticket
+          </h3>
+        </div>
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-heart text-[#696969]"></i>
+          <h3 style={style} className="text-[#696969]">
+            Favorites
+          </h3>
+        </div>
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-cog text-[#696969]"></i>
+          <h3 style={style} className="text-[#696969]">
+            setting
+          </h3>
+        </div>
+      </div>
+
+      <div className="pl-10">
+        <div className="flex gap-3 py-2 items-center">
+          <i className="fas fa-sign-out-alt text-[#696969]"></i>
+          <h3 style={style} className="text-[#696969]">
+            Logout
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -1,87 +1,162 @@
-import React, { useState } from "react";
-import { searchData } from "../data";
-import Pagination from "./Pagination";
+import React from "react";
 
 const ResultsSection = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(4);
-
-  const lastPostIndex = currentPage * postsPerPage;
-  const firstPostIndex = lastPostIndex - postsPerPage;
-  const searchPage = searchData.slice(firstPostIndex, lastPostIndex);
-
   return (
-    <div className="w-[100%] lg:w-[65%] p-8 bg-[#E9F0F8]">
-      <div className="flex justify-between">
-        <div>
-          <h2 className="lg:text-[35px] text-[25px] text-[#636E8B]">
-            Search Results
-          </h2>
-          <p className="text-[#bbbfd8]">We found 15 results</p>
-        </div>
-        <div className="text-center lg:text-start">
-          <button className="bg-white my-2 lg:my-0 hover:bg-[#6682e3] hover:text-white text-[#C8CCE0]  py-2 px-4 rounded-[30px] mx-2">
-            CHEAPEST
-          </button>
-          <button className="bg-white my-2 lg:my-0 hover:bg-[#6682e3] hover:text-white text-[#C8CCE0]  py-2 px-4 rounded-[30px] mx-2">
-            SHORTEST
-          </button>
-          <button className="bg-white my-2 lg:my-0 hover:bg-[#6682e3] hover:text-white text-[#C8CCE0]  py-2 px-4 rounded-[30px] mx-2">
-            RECOMENDED
-          </button>
-        </div>
+    <div className="w-[100%] lg:w-[80%] p-8 bg-[#ffffff]">
+      <div className="relative flex items-center border rounded-md p-2">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full outline-none focus:ring focus:ring-blue-300 rounded-md pr-10"
+        />
+
+        <input
+          type="date"
+          value="2023-05-03"
+          className="absolute right-0 top-2 focus:ring focus:ring-blue-300"
+        />
+
+        <i className="fas fa-bell absolute right-3 top-4 text-gray-600 text-[25px]"></i>
       </div>
-      <div className="flex items-center py-4 ">
-        <h2 className="text-[#697490] lg:text-[30px] text-[20px] pr-8 font-bold">
-          London
-        </h2>
-        <i className="fas fa-arrow-right text-[#9fa5c4] text-[20px]"></i>
-        <h2 className="text-[#697490] lg:text-[30px] text-[20px] pl-8 font-bold">
-          New York
-        </h2>
-      </div>
+
+      <header className="section__container header__container">
+        <div className="header__image__container">
+          <div className="header__content"></div>
+          <div className="booking__container">
+            <form>
+              <div className="form__group">
+                <div className="input__group">
+                  <input type="text" />
+                  <label>Location</label>
+                </div>
+                <p>Where are you going?</p>
+              </div>
+              <div className="form__group">
+                <div className="input__group">
+                  <input type="text" />
+                  <label>Check In</label>
+                </div>
+                <p>Add date</p>
+              </div>
+
+              <div className="form__group">
+                <div className="input__group">
+                  <input type="text" className="outline-none" />
+                  <label>Guests</label>
+                </div>
+                <p>Add guests</p>
+              </div>
+            </form>
+            <button className="btn">
+              <i className="ri-search-line"></i>
+            </button>
+          </div>
+        </div>
+      </header>
+
       <div>
-        {searchPage.map((ele) => {
-          return (
-            <div
-              key={ele.id}
-              className="flex bg-white justify-between items-center py-3 px-8 rounded-[15px] my-3 hover:shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px]"
-            >
-              <img src={ele.airline} width="100" height="100" alt="" />
-              <div className="lg:flex items-center gap-9 hidden">
-                <div>
-                  <p className="text-[#b6bed5]">Depart</p>
-                  <p className="text-[#636E8B]">{ele.departureDate}</p>
-                  <p className="text-[#697490] font-bold">
-                    {ele.departureTime}
-                  </p>
-                </div>
-                <div className="flex flex-col">
-                  <button className="bg-[#484862]  text-white  py-1 px-1 rounded-[30px] mx-2">
-                    {`${ele.stops} Stop`}
-                  </button>
-                  <p className="text-[#b6bed5]">{ele.hours}</p>
-                </div>
-                <div>
-                  <p className="text-[#b6bed5]">Arrive</p>
-                  <p className="text-[#636E8B]">{ele.arriveDate}</p>
-                  <p className="text-[#697490] font-bold">{ele.arriveTime}</p>
-                </div>
-              </div>
-              <div className="">
-                <p className="text-[#b6bed5]">Price</p>
-                <p className="text-[#697490] font-bold text-[30px]">{`₦ ${ele.price}K`}</p>
-              </div>
+        <h2 className="text-[#303555] font-bold">Trending Destination</h2>
+        <div className="lg:grid lg:grid-cols-3 gap-2 ">
+          <div className="max-w-sm flex rounded overflow-hidden shadow-lg my-6 lg:my-0">
+            <img
+              src="https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              alt="Item 1"
+              className="w-[100px] h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
             </div>
-          );
-        })}
+          </div>
+
+          <div className="max-w-sm flex rounded overflow-hidden shadow-lg   my-6 lg:my-0">
+            <img
+              src="https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80"
+              alt="Hotel 1"
+              className="w-[100px] h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+
+          <div className="max-w-sm flex rounded overflow-hidden shadow-lg  my-6 lg:my-0">
+            <img
+              src="https://images.unsplash.com/photo-1606046604972-77cc76aee944?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=80"
+              alt="Hotel 1"
+              className="w-[100px] h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <Pagination
-        totalPosts={searchData.length}
-        postsPerPage={postsPerPage}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      />
+
+      <div>
+        <div className="flex justify-between pt-4">
+          <h2 className="text-[#303555] font-bold">Best Offer</h2>
+          <h3 className="text-[#303555]">View All</h3>
+        </div>
+        <div className="grid lg:grid-cols-4 grid-cols-2 space-x-4">
+          <div className="max-w-sm  rounded overflow-hidden shadow-lg">
+            <img
+              src="https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              alt="Item 1"
+              className="w-full h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="https://images.unsplash.com/photo-1606046604972-77cc76aee944?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=80"
+              alt="Hotel 1"
+              className="w-full h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+
+          <div className="max-w-sm  rounded overflow-hidden shadow-lg">
+            <img
+              src="https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              alt="Hotel 1"
+              className="w-full h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+
+          <div className="max-w-sm  rounded overflow-hidden shadow-lg">
+            <img
+              src="https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              alt="Hotel 1"
+              className="w-full h-[100px] p-1 rounded-lg"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Name of Hotel 1</div>
+              <p className="text-gray-700 text-base">Description of Hotel 1</p>
+              <p className="text-gray-800 text-xl mt-2">Price: $19.99</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
